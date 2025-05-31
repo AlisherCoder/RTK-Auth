8fa5edd2a254d3a1ff1715ad364fd602f788032b
Alisher <alishersharipov670@gmail.com>
2025-05-31 21:56:25 +0500

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React, { lazy } from "react";
-import { Link, useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
+import ResponsiveAppBar from "./components/header/Header";
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const OTP = lazy(() => import("./pages/OTP"));
 const Register = lazy(() => import("./pages/Register"));
 const Profil = lazy(() => import("./pages/Profil"));
+const About = lazy(() => import("./pages/About"));
 
 const App = () => {
    return (
       <>
-         <Link to={"/"}>Home</Link>
+         {/* <Link to={"/"}>Home</Link>
          <Link to={"/otp"}>Register</Link>
+         <Link to={"/about"}>About</Link> */}
+         <ResponsiveAppBar />
          {useRoutes([
             { path: "/", element: <Home /> },
             { path: "/login", element: <Login /> },
             { path: "/register", element: <Register /> },
             { path: "/otp", element: <OTP /> },
             { path: "/profil", element: <Profil /> },
+            { path: "/about", element: <About /> },
          ])}
       </>
    );
